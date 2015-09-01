a1bb01a133586cd4f08858019104be2a22dc7f4c
pmsg863 <pmsg863@gmail.com>
2015-09-01 17:12:50 +0800

    新增vertx项目和scala特性学习

--- vertx3test/src/main/java/com/xmgps/yfzx/hwb/scala/ScalaTest.scala
+++ vertx3test/src/main/java/com/xmgps/yfzx/hwb/scala/ScalaTest.scala
@@ -1,5 +1,8 @@
 package com.xmgps.yfzx.hwb.scala
 
+import scala.collection.convert.Wrappers.ConcurrentMapWrapper
+import scala.collection.immutable.{HashSet, HashMap}
+
 /* An object with a main method is a program. */
 
 object ScalaTest {
@@ -12,6 +15,39 @@ object ScalaTest {
     // case blocks also act as functions:
     val myCaseID : Int => Int = {
       case x => x+3
+    }
+  }
+
+  class CollTest{
+    def printArray (): Unit = {
+      var array = new Array[String](3)
+      array(0)="hello"
+      array(1)="my"
+      array(2)="world"
+      for(i<-0 to 2){
+        println(array(i))
+      }
+    }
+
+    def printMap (): Unit ={
+      var map = HashMap(1->"a",2->"b",3->"c")
+      map.+(4->"d")
+      map +(5->"e")
+      map.foreach(value=>print(value+" "))
+    }
+
+    def printList (): Unit ={
+      var list = List("111","222","333")
+      var list2 = List("aaa","bbb")
+      list2 +("ccc")
+      var list3 = list ::: list2
+      list3.foreach(value=>print(value+" "))
+    }
+
+    def printSet (): Unit = {
+      var set = HashSet("1","2","3");
+      set+="4"
+      set.foreach(value=>print(value+" "))
     }
   }
 
@@ -71,6 +107,13 @@ object ScalaTest {
     val anStringArray = Array("1","2","3","4","5")
     val myArgs = new Array[String](anStringArray.length)
     (0 until args.length) foreach (i => myArgs(i) = anStringArray(i))
+
+    var collTest = new CollTest
+    collTest.printArray()
+    collTest.printList()
+    collTest.printMap()
+    collTest.printSet()
+
   }
 
 }
